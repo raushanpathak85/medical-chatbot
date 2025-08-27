40461842fd5a620e724ea99cd85232ea4cb0f0a8
Raushan Pathak <raushanpathak85@gmail.com>
2025-08-27 10:57:26 +0530

    Files and folders added successfully.

---
+++ template.sh
@@ -0,0 +1,16 @@
+## Create the Folder
+mkdir -p src
+mkdir -p research
+
+## Creating the files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trial.ipynb
+touch requirement.txt
+
+echo "Directory and files created successfully."
+
