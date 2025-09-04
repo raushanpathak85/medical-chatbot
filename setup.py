c2fe94e756a0a7a64773157e4978a3c549fcf8aa
Raushan Pathak <raushanpathak85@gmail.com>
2025-09-04 14:24:11 +0530

    add files and folder

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name= "Medical ChatBot",
+    version= "0.1.0",
+    author= "Raushan Pathak",
+    author_email= "raushanpathak85@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    
+)
